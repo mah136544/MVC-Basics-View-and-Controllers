4a42b38454d13d045bd7baa41d9537e7b295968a
mah136544 <mahnoushaminshahidi@gmail.com>
2021-11-12 15:15:32 +0100

    I Create new Controller named DoctorController

---
+++ MVC Basics View and Controllers/Controllers/DoctorController.cs
@@ -0,0 +1,77 @@
+﻿using Fever.Models;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+//namespace MVC_Basics_View_and_Controllers.Controllers
+namespace Fever.Controllers
+{
+    public class DoctorController : Controller
+    {
+
+
+        [Route("FeverCheck")]
+        [HttpGet]
+        public ActionResult FeverChecker()
+        {
+            ViewData["Fever"] = "";
+            ViewData["shypothermia"] = "";
+            return View();
+        }
+
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        [Route("FeverCheck")]
+        public ActionResult FeverChecker(FeverModel model)
+        {
+            if (model.Unit == "Fahrenheit")
+            {
+                if (model.CheckFever >= 99)
+                {
+                    ViewData["Message"] = "You have fever ";
+                }
+                else
+                {
+                    ViewData["Message"] = "You have not  fever ";
+                    if (model.Ishypothermia && (model.CheckFever <= 95))
+                    {
+                        ViewData["shypothermia"] = "but hypothermia";
+                    }
+                    else
+                    {
+                        ViewData["shypothermia"] = "";
+
+                    }
+                }
+            }
+            else
+            {
+                if (model.CheckFever >= 38)
+                {
+                    ViewData["Message"] = "You have fever ";
+                }
+                else
+                {
+                    ViewData["Message"] = "You have not  fever ";
+                    if (model.Ishypothermia && (model.CheckFever <= 35))
+                    {
+                        ViewData["shypothermia"] = "but hypothermia";
+                    }
+                    else
+                    {
+                        ViewData["shypothermia"] = "";
+                    }
+
+                }
+
+            }
+            return View();
+        }
+    }
+}
+
+
---
+++ MVC Basics View and Controllers/Models/Fever.cs
@@ -0,0 +1,18 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+
+
+namespace Fever.Models
+{
+    public class FeverModel
+    {
+        [Required(ErrorMessage = "Please enter tempure level")]
+        public float CheckFever { get; set; }
+        public string Unit { get; set; }
+        public bool Ishypothermia
+        {
+            get; set;
+
+        }
+}
+}
